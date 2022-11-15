788daa1872c0727b45af9ebe160fc689af53bd18
kpi-jps <jps.spj@gmail.com>
2022-11-15 16:14:11 -0300

    first commit

---
+++ app/src/main/java/ifsp/ads/pdm/jp/splitthebill/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package ifsp.ads.pdm.jp.splitthebill.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ifsp.ads.pdm.jp.splitthebill.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
